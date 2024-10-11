be6559152c96a5c642ac5f030c2e0aee5573fcba
Kirill <kirilll.bychkov@gmail.com>
2024-10-11 21:31:29 +0700

    add Sound button

--- config/ThemeProvider.js
+++ config/ThemeProvider.js
@@ -1,32 +1,32 @@
 import { lightColors, darkColors } from "./theme";
 import { useColorScheme } from "react-native";
-import React, { createContext, useContext, useEffect, useState} from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext({
     dark: false,
     colors: lightColors,
-    setScheme: ()=>{}
-})
+    setScheme: () => { }
+});
 
 export const ThemeProvider = (props) => {
-    const colorSheme = useColorScheme(colorSheme == "dark");
-    const [isDark, setIsDark] = useState(colorSheme == "dark");
+    const colorScheme = useColorScheme();
+    const [isDark, setIsDark] = useState(colorScheme === "dark");
 
-    useEffect(()=>{
-        setIsDark(colorSheme == "dark");
-    },[colorSheme])
+    useEffect(() => {
+        setIsDark(colorScheme === "dark");
+    }, [colorScheme]);
 
     const defaultTheme = {
         dark: isDark,
         colors: isDark ? darkColors : lightColors,
-        setScheme: (scheme)=>setIsDark(scheme === "dark")
-    }
+        setScheme: (scheme) => setIsDark(scheme === "dark")
+    };
 
     return (
         <ThemeContext.Provider value={defaultTheme}>
             {props.children}
         </ThemeContext.Provider>
-    )
-}
+    );
+};
 
-export const useTheme = ()=>useContext(ThemeContext)
+export const useTheme = () => useContext(ThemeContext);
