e9ae7f89a53865279f27502a94cb03ea15f7c7b7
Kirill <kirilll.bychkov@gmail.com>
2024-10-12 12:37:36 +0700

    update project

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,5 @@
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ScrollView, TouchableOpacity } from 'react-native';
 import { useTheme } from '../config/ThemeProvider';
-
 
 export default function App() {
     const { colors } = useTheme();
@@ -9,6 +7,12 @@ export default function App() {
     return (
         <ScrollView style={[styles.container, { backgroundColor: colors.background }]}>
             <StatusBar backgroundColor={colors.background} />
+            {/* Блоки для разных тем изучения Python */}
+            <View style={styles.blocksContainer}>
+                <TouchableOpacity style={[styles.block, { backgroundColor: colors.primary }]}>
+                    <Text style={[styles.blockText, { color: colors.text }]}>Типы данных</Text>
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     );
 }
@@ -16,5 +20,24 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    blocksContainer: {
+        padding: 20,
+    },
+    block: {
+        borderRadius: 10,
+        padding: 20,
+        marginBottom: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5, // для тени на Android
+    },
+    blockText: {
+        fontSize: 18,
+        fontFamily: 'Rubik-Bold',
     },
 });
